080951776a26d6a09388f8dfb13ee2946039bb22
Max Masse <maxmasse199@gmail.com>
2024-01-16 16:45:08 -0500

    installation de nodemon

--- node-pokemon-api/app.js
+++ node-pokemon-api/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello Express"));
+app.get("/", (req, res) => res.send("Hello Express, 5"));
 
 app.listen(port, () =>
   console.log(
