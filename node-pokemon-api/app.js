823b9aba7e08a957c96e16ac3bea52ccf1848622
Max Masse <maxmasse199@gmail.com>
2024-01-17 16:29:33 -0500

    réponse JSON

--- node-pokemon-api/app.js
+++ node-pokemon-api/app.js
@@ -4,21 +4,20 @@ let pokemons = require("./mock-pokemon");
 const app = express();
 const port = 3000;
 
-// app.use(cors())
-
 app.get("/", (req, res) => res.send("Hello Express, 6"));
 
 app.get("/api/pokemons/:id", (req, res) => {
   // retourne ici le noombre et non la chaine de caracères
   const id = parseInt(req.params.id);
   const pokemon = pokemons.find((pokemon) => pokemon.id === id);
-  var total= pokemons.length
-//   res.send(`Vous avez-demandé le pokémon ${pokemon.name}` );
-  res.send(`il y a ${total} pokemon dans votre pokedex`);
-
+  var total = pokemons.length;
+  res.json(pokemon);
 });
 
-
+app.get("/api/pokemons/", (req, res) => {
+  var total = pokemons.length;
+  res.send(`il y a ${total} pokemon dans votre pokedex`);
+});
 
 app.listen(port, () =>
   console.log(
