37f04d0882a810c84a93f50ab708029921df58a1
Max Masse <maxmasse199@gmail.com>
2024-01-17 13:45:52 -0500

    mise en place de webpack

--- node-pokemon-api/app.js
+++ node-pokemon-api/app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello Express, 5"));
+app.get("/", (req, res) => res.send("Hello Express, 6"));
+
+app.get('/api/pokemons/1',(req,res)=>res.send("hello, Bulbozar"))
 
 app.listen(port, () =>
   console.log(
